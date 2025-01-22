78f73906ac277ef2bd340707108eee945356d628
melvinantonioo <melvin.antonio88@gmail.com>
2025-01-22 23:32:30 +0700

    feat: testing-1

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,3 +1,33 @@
+import Container from "./Container";
+import Logo from "./Logo";
+import Search from "./Search";
+import UserMenu from "./UserMenu";
+
 export const Header = () => {
-  return <div>Header</div>;
+  return <div>
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+
+        <Container>
+
+          <div
+            className="
+              flex
+              flex-row
+              justify-between
+              gap-3
+              md:gap-0
+          "
+          >
+
+            <Logo />
+            <Search />
+            <UserMenu />
+
+          </div>
+
+        </Container>
+      </div>
+    </div>
+  </div>;
 };
---
+++ apps/web/src/components/Logo.tsx
@@ -0,0 +1,24 @@
+"use client";
+import React from 'react'
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+
+const Logo = () => {
+    // const router = useRouter();
+
+    return (
+        // <Image
+        //     alt="logo"
+        //     className='hidden md:block cursor-pointer'
+        //     height="100"
+        //     width="100"
+        //     src=""
+        // />  download logo 
+
+        <div className='hidden md:block cursor-pointer'>
+            Logo
+        </div>
+    )
+}
+
+export default Logo
